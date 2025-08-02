2da1b6ffd6a23d935d82f2a9a21363f0ca5c3645
jhbrunokim <bobhappy@naver.com>
2025-08-03 02:51:04 +0900

    merge fix

--- Athena.Cache.Core/Enums/InvalidationType.cs
+++ Athena.Cache.Core/Enums/InvalidationType.cs
@@ -5,10 +5,18 @@
 /// </summary>
 public enum InvalidationType
 {
-    /// <summary>연관된 모든 캐시 삭제</summary>
-    All,
+    /// <summary>테이블 기반 무효화</summary>
+    Table,
+    
     /// <summary>패턴에 맞는 캐시만 삭제</summary>
     Pattern,
+    
+    /// <summary>배치 무효화</summary>
+    Batch,
+    
     /// <summary>관련 테이블들의 캐시도 함께 삭제</summary>
-    Related
+    Related,
+
+    /// <summary>연관된 모든 캐시 삭제</summary>
+    All
 }
